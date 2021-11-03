6f5e09d3c5477c9c1146b07b51efdacc70b76aac
nancywan1004 <leqiwan@alumni.ubc.ca>
2021-11-02 20:09:04 -0700

    11 files deleted

--- apis/schemas/card.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-const card = new mongoose.Schema({
-    name: String,
-    url: String,
-    series: String
-});
-
-module.exports = mongoose.model("Card", card);
